989fa7eef802497c62cc02a87b8de4a4f71c7a1c
jannettim <jannettim@gmail.com>
2018-05-08 20:50:30 -0400

    Update introduction text

--- app/instructions.py
+++ app/instructions.py
@@ -1,4 +1,4 @@
-from bokeh.models import ColumnDataSource, Paragraph
+from bokeh.models import ColumnDataSource, Paragraph, Div
 from bokeh.layouts import widgetbox, layout
 
 
@@ -6,72 +6,55 @@ p_width = 500
 header_font_size = 21
 p_font_size = 12
 
-open = Paragraph(text="Instructions for Decision Tool", width=p_width, style={"font-size": "{}pt".format(header_font_size)})
+open = Paragraph(text="Introduction for QDAS Assessment Toolkit", width=p_width, style={"font-size": "{}pt".format(header_font_size)})
 
-p1 = Paragraph(text="This tool will help you determine which of your criteria is most important and which decision "
-                    "will best meet those criteria. There are 4 parts to this tool. First is the rubric, second is the "
-                    "features checklist, third is the multicriteria decision making model (MCDM), and fourth are short "
-                    "briefs on the tools analyzed.", width=p_width,
-               style={"font-size": "{}pt".format(p_font_size)})
+intro_text = Div(text="""
+This microsite provides a toolkit for comparing the pros and cons of 7 qualitative data analysis options:<br>
+<blockquote>
+&#8226 Atlas.ti<br>
+&#8226 Dedoose<br>
+&#8226 MAXQDA<br>
+&#8226 Nvivo<br>
+&#8226 QDA Miner<br>
+&#8226 TOM <br>
+&#8226 Transana<br>
+</blockquote>
 
-h1 = Paragraph(text="Rubric", width=p_width, style={"font-size": "{}pt".format(header_font_size)})
+The toolkit includes 4 tools:<br>
+<blockquote>
+1. <b>Briefs</b> on 7 QDA software options<br>
+2. A comprehensive, comparative <b>Features Checklist</b><br>
+3. A <b>Scoring Rubric</b> for scoring performance on 16 dif)ferentiating features<br>
+4. A <b>Multi-Criteria Decision-Making Model (MCDM</b> for helping the decision maker determine which features they value most, and which software is best based on their values<br>
+</blockquote>
+For more information, including a step-by-step tutorial for using the MCDM model please see the User Guide, here. Otherwise, a brief overview of the MCDM model is provided below.
+""", width=p_width, style={"font-size": "{}pt".format(p_font_size)})
 
-p2 = Paragraph(text="The rubric shows and defines the criteria used in your model. Each criteria is graded on a scale "
-                    "from excellent to poor. Hover over each box to show the definition of each grade. This "
-                    "will allow users to see the variation of each tools across your criteria and make "
-                    "comparisons across all tools easy.", width=p_width,
-               style={"font-size": "{}pt".format(p_font_size)})
+h1 = Paragraph(text="Multi-Criteria Decision Making (MCDM) Model", width=p_width,
+               style={"font-size": "{}pt".format(header_font_size)})
 
-h2 = Paragraph(text="Features Checklist", width=p_width, style={"font-size": "{}pt".format(header_font_size)})
-
-p3 = Paragraph(text="The features checklist shows a detailed list of the features for each tool you are comparing. "
-                    "Again, hover over the dots to see the feature definitions.", width=p_width,
-               style={"font-size": "{}pt".format(p_font_size)})
-
-h3 = Paragraph(text="Multi-Criteria Decision Making (MCDM) Model", width=p_width, style={"font-size": "{}pt".format(header_font_size)})
-
-p4 = Paragraph(text="The MCDM model helps you make your final decision regarding which tool you should use. To use "
-                    "this tab follow these instructions:", width=p_width, style={"font-size": "{}pt".format(p_font_size)})
-
-p5 = Paragraph(text="1) Choose the subset of criteria that are most important to you from the box. Click 'Submit "
-                    "Criteria' when you have finished.", width=p_width, style={"font-size": "{}pt".format(p_font_size)})
-
-p6 = Paragraph(text="2) Rank each scenario generated with a unique number. Lower numbers mean that the scenario "
-                    "is more important. Be sure to pay attention to the Swing Weighting Matrix on the bottom of the "
-                    "page. This will help you define each scenarios if you hover over the boxes. You should rank each "
-                    "scenario such that the criteria that is ranked as best will have the best possible features for "
-                    "that criteria but in all other criteria you will get the worst possible outcome. Click 'Calculate "
-                    "Ranks' when you have finished.", width=p_width, style={"font-size": "{}pt".format(p_font_size)})
-
-p7 = Paragraph(text="3) Use the sliders that appear to tell the tool how important that criterion is relative to "
-                    "the others you have selected. The criteria you rank 1st will have a weight of 1 and this cannot "
-                    "be changed. The remaining criteria will be given a default weight that you should alter. You "
-                    "should weight each criteria so that it reflects how important that criterion is to you should you "
-                    "ONLY be able to have the best outcome for that criterion but the worst in every other criteria. "
-                    "Please refer to the Swing Weighting Matrix for the definitions of best and worst for each "
-                    "criterion. The weights will be between 0 and 1. Click 'Update Model' when finished.",
-               width=p_width, style={"font-size": "{}pt".format(p_font_size)})
-p8 = Paragraph(text="4) A new table will appear with the tools ranks from best to worst with their final scores. "
-                    "Use this to make your decision.", width=p_width, style={"font-size": "{}pt".format(p_font_size)})
-
-h4 = Paragraph(text="Briefs", width=p_width, style={"font-size": "{}pt".format(header_font_size)})
-
-p9 = Paragraph(text="Briefs contain short descriptions of the tool, detailed cost information, and screenshots of "
-                    "the tool.", width=p_width, style={"font-size": "{}pt".format(p_font_size)})
-
-
+mcdm_text = Div(text="""
+<b>Step 1</b> Choose which of the 16 criteria is important to you:
+<blockquote>
+-       <b>Review</b> the best and worst performance on each criterion to understand the range of capabilities associated with each feature. These are listed on the next page and not on the website. Refer to this list as you complete the MCDM exercise<br>
+-       To <b>select</b> multiple criteria, hold down the control key<br>
+-       <b>Hit</b> ‘submit criteria’ after you’ve finished selecting all of the criteria you value<br>
+</blockquote>
+<b>Step 2</b> Rank Each Criterion using Swing-Weighting:
+<blockquote>
+-       <b>Review</b> each ‘best on one, worse on all else” scenarios. The best and worst values are shown in the tooltips by hovering over each cell<br>
+-       <b>Rank</b> each scenario using the rank dropdowns<br>
+-       <b>Hit</b> ‘calculate ranks’ to advance to the next step<br>
+</blockquote>
+<b>Step 3</b> Weight each scenario
+<blockquote>
+-       Use the slider to <b>weight</b> each scenario. The scenario ranked as 1 is automatically weighted at 100. Each successively ranked scenario is weighted can be weighted as less than or equal to the scenario preceding it. How much less preferable is this scenario in relation to this first? All weights are in relation to the<br>
+-       Hit ‘update model’<br>
+</blockquote>
+<b>Step 4</b> Review results
+""", width=p_width, style={"font-size": "{}pt".format(p_font_size)})
 
 app_layout = layout([open,
-                     p1,
+                     intro_text,
                      h1,
-                     p2,
-                     h2,
-                     p3,
-                     h3,
-                     p4,
-                     p5,
-                     p6,
-                     p7,
-                     p8,
-                     h4,
-                     p9])
+                     mcdm_text])
